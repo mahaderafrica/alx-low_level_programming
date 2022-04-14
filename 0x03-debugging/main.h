dd8229452435b5ca6fd097323291b5c8ca554f5d
mahaderafrica <mahaderafrica131621@gmail.com>
2022-04-13 21:03:36 -0700

    task two

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,13 +7,6 @@ void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
+void print_times_table(int n);
 
 #endif
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-[A[C[C[C[C[C[C
->>>>>>> a04b89505de7c48bebb2b1788634d5bcab705cc3
-=======
-[A[C[C[C[C[C[C
->>>>>>> 3051e7258fd92f325132f6150d13fe13ab360a3f
